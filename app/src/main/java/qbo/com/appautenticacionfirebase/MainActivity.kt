de7ee5db9ac42f2337074d2a8ca5b33cb9e37f93
LuisAngel <luisangel.salvat@gmail.com>
2020-10-08 20:59:47 -0500

    3. parte de Autenticación Firebase

--- app/src/main/java/qbo/com/appautenticacionfirebase/MainActivity.kt
+++ app/src/main/java/qbo/com/appautenticacionfirebase/MainActivity.kt
@@ -108,6 +108,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data)
